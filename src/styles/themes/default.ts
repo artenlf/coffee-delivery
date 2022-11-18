b157e4df2f50cdf52af21ca3298b7e745dafb427
artenlf <felipe.arten@gmail.com>
2022-11-18 12:39:29 -0300

    chore: implemented global theme and styles

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  'bg-color': '#FAFAFA',
+
+  'gray-100': '#F3F2F2',
+  'gray-200': '#EDEDED',
+  'gray-300': '#E6E5E5',
+  'gray-400': '#D7D5D5',
+  'gray-600': '#8D8686',
+  'gray-700': '#574F4D',
+  'gray-800': '#403937',
+  'gray-900': '#272221',
+
+  'purple-100': '#EBE5F9',
+  'purple-500': '#8047F8',
+  'purple-700': '#4B2995',
+
+  'yellow-100': '#F1E9C9',
+  'yellow-600': '#DBAC2C',
+  'yellow-700': '#C47F17',
+
+  white: '#FFF',
+};
