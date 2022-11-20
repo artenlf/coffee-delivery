febe3c542cbbae66fb7b63542277f1a2cdb2d822
artenlf <felipe.arten@gmail.com>
2022-11-20 00:34:19 -0300

    feat: created Intro section of app

---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,72 @@
+import styled from 'styled-components';
+
+export const IntroContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 5.75rem 10rem;
+
+  .title__container {
+    width: 36.75rem;
+  }
+
+  img {
+    max-height: 22.5rem;
+    max-width: 29.75rem;
+  }
+`;
+
+export const IntroTitle = styled.h1`
+  color: ${(props) => props.theme['gray-900']};
+  font-family: 'Baloo 2', cursive;
+  font-size: 3rem;
+  font-weight: 800;
+`;
+
+export const IntroSubTitle = styled.h2`
+  color: ${(props) => props.theme['gray-800']};
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.25rem;
+  font-weight: 400;
+  margin: 1rem auto 4.125rem auto;
+`;
+
+export const IntroItemsContainer = styled.div`
+  display: grid;
+  gap: 1.25rem 0;
+  grid-template-columns: repeat(2, 1fr);
+
+  p {
+    align-items: center;
+    display: flex;
+  }
+
+  .cart__container,
+  .package__container,
+  .timer__container,
+  .coffee__container {
+    align-items: center;
+    border-radius: 999px;
+    color: ${(props) => props.theme['bg-color']};
+    display: flex;
+    height: 2rem;
+    justify-content: center;
+    width: 2rem;
+    margin-right: 0.75rem;
+  }
+
+  .cart__container {
+    background: ${(props) => props.theme['yellow-700']};
+  }
+
+  .package__container {
+    background: ${(props) => props.theme['gray-700']};
+  }
+
+  .timer__container {
+    background: ${(props) => props.theme['yellow-600']};
+  }
+
+  .coffee__container {
+    background: ${(props) => props.theme['purple-500']};
+  }
+`;
