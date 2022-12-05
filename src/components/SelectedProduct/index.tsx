8385c410aa1fa223c2ea545f3d44e3027dd2cff5
artenlf <felipe.arten@gmail.com>
2022-12-04 22:28:50 -0300

    feat: empty cart message

--- src/components/SelectedProduct/index.tsx
+++ src/components/SelectedProduct/index.tsx
@@ -9,26 +9,28 @@ export function SelectedProduct(props: any) {
   const { cartItems } = props;
   return (
     < SelectedProductContainer >
-      {cartItems.length === 0 && <div>O seu carrinho está vázio</div>}
-      <div className='product__wrapper'>
-        <img src={Expresso} alt="" />
-        <div className="controls__wrapper">
-          <h3>Expresso Tradicional</h3>
-          <div className="buttons__wrapper">
-            <Controls />
+      {cartItems.length < 1 ? <div> O seu carrinho está vázio. Adicione um café!</div> :
+        <>
+          <div className='product__wrapper'>
+            <img src={Expresso} alt="" />
+            <div className="controls__wrapper">
+              <h3>Expresso Tradicional</h3>
+              <div className="buttons__wrapper">
+                <Controls />
 
-            <div className="controls">
-              <button>
-                <Trash size={16} className="trash-icon" />
-                Remover
-              </button>
+                <div className="controls">
+                  <button>
+                    <Trash size={16} className="trash-icon" />
+                    Remover
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <span className='price'>R$ 19,80</span>
-
+          <span className='price'>R$ 19,80</span>
+        </>
+      }
     </SelectedProductContainer >
 
   )
