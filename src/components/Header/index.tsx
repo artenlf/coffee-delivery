178d67c21c25e57d8db303711d40cc0428f07930
artenlf <felipe.arten@gmail.com>
2022-12-23 23:27:03 -0300

    feat: city/state field now updates with given address

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { MapPin, ShoppingCart } from 'phosphor-react';
 import { NavLink } from 'react-router-dom';
+import { useAddress } from '../../contexts/AddressContext';
 import { useCart } from '../../contexts/CartContext';
 import { HeaderContainer } from './styles';
 import logo from '/logo.svg';
@@ -7,6 +8,7 @@ import logo from '/logo.svg';
 export function Header() {
 
   const { cartQuantity } = useCart();
+  const { addressFields } = useAddress();
 
   return (
     <HeaderContainer>
@@ -17,7 +19,9 @@ export function Header() {
         <NavLink to="/checkout" title="location">
           <div className="map">
             <MapPin size={22} weight="fill" className="map-icon" />
-            <p>Cidade/UF</p>
+            {
+              <p>{`${addressFields.city}/${addressFields.state}`}</p>
+            }
           </div>
         </NavLink>
         <NavLink to="/checkout" title="cart">
